56a3387bebebea2093bcc6e42f0410ed09debc5f
Jan Derendinger <derendingerjan@gmail.com>
2023-06-19 19:59:56 +0200

    deleted reminders and modify handler

--- backend/pkg/controller/handler.go
+++ backend/pkg/controller/handler.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"log"
-	"reminders/pkg/repository"
+	"recipies/pkg/repository"
 )
 
 type Handler struct { // Objekt Handler
@@ -16,16 +16,16 @@ func New() *Handler {
 		engine:     gin.Default(), // Eigenschaften auf Zeilen 10 + 11 definiert.
 		repository: repository.New(),
 	}
-	router := h.engine.Group("/api/reminder/") // Gruppe definiert, in dieser Gruppe folgende Endpunkte definiert, damit ich die Service aufrufen kann. h ist das objekt, welches ich oben instanziert habe.
-	router.GET("/:id", h.GetReminder)          // Punkt ist immer von einem Objekt, welches instanziert wurde. damit kann z.B. auf die Eigenschaften, Function oder Services zugegriffen werden.
-	router.DELETE("/:id", h.DeleteReminder)    // Rauts definieren
-	router.GET("/all", h.GetReminders)
-	router.POST("/", h.CreateReminder)
-	router.PUT("/", h.UpdateReminder)
+	router := h.engine.Group("/api/recipe/") // Gruppe definiert, in dieser Gruppe folgende Endpunkte definiert, damit ich die Service aufrufen kann. h ist das objekt, welches ich oben instanziert habe.
+	router.GET("/:id", h.GetRecipe)          // Punkt ist immer von einem Objekt, welches instanziert wurde. damit kann z.B. auf die Eigenschaften, Function oder Services zugegriffen werden.
+	router.DELETE("/:id", h.DeleteRecipe)    // Rauts definieren
+	router.GET("/all", h.GetRecipes)
+	router.POST("/", h.CreateRecipe)
+	router.PUT("/", h.UpdateRecipe)
 	return h
 }
 
-func (h Handler) Run() { // Func welches an Objekt Handler gebunden ist.
+func (h *Handler) Run() { // Func welches an Objekt Handler gebunden ist.
 	err := h.engine.Run()
 	if err != nil {
 		log.Fatalln(err)
